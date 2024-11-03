c1ec987e77496909d70b5967f9adb48d4a50d008
Jelenal1 <jabbruh10@gmail.com>
2024-11-03 18:01:57 +0100

    chore: added more content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,40 +3,35 @@ import DVDKachel from "@/components/DVDKachel";
 import Terminal from "@/components/Terminal";
 import { BiLogoJavascript, BiLogoTypescript } from "react-icons/bi";
 import { FaJava } from "react-icons/fa";
+import { SiNextdotjs } from "react-icons/si";
 
 export default function Home() {
   return (
-    <main className="flex h-full flex-col overflow-hidden">
-      <Terminal delay={60}>
-        <div>
-          Hi there, I&apos;m Jelena <br />
-          I&apos;m a frontend developer who loves to learn new things.
-          <br />
-          One example is this page, it&apos;s self hosted on a vps with coolify.
-          <br />
-          <br />
-          So feel free to take a look around and contact me. <br />
-          <br />
-        </div>
-        The stuff I work with in my free time
+    <main className="flex h-full flex-col overflow-hidden pt-10">
+      <Terminal delay={50}>
+        Hi there, I&apos;m Jelena <br />
+        I&apos;m a frontend developer who loves to learn new things.
+        <br />
+        One example is this page, it&apos;s self hosted on a vps with coolify.
+        <br />
+        <br />
+        So feel free to take a look around and contact me. <br />
+        <br />
         <div className="flex h-72 w-full border">
           <DVDKachel
             icons={[
-              <div key={0} className="min-w-40">
-                <BiLogoJavascript size={50} />
-                <div>JavaScript</div>
-              </div>,
-              <div key={1} className="min-w-40">
-                <BiLogoTypescript size={50} />
-                <div>TypeScript</div>
-              </div>,
-              <div key={2} className="min-w-40">
-                <FaJava size={50} />
-                <div>Java</div>
-              </div>,
+              <BiLogoJavascript key={0} size={50} />,
+
+              <BiLogoTypescript key={1} size={50} />,
+
+              <FaJava key={2} size={50} />,
+
+              <SiNextdotjs key={3} size={50} />,
             ]}
           />
         </div>
+        <br />
+        Follow the white rabbit.
       </Terminal>
     </main>
   );
